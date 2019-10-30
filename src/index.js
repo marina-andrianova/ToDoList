ed67e6b6b570798039902577283d2db635ff7489
Marina <marinaandrianova.exceedteam@gmail.com>
2019-10-30 16:43:35 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-const el = (
-    <div>
-        <h1>My ToDo List</h1>
-        <input placeholder="search"/>
+
+const TodoList = () => {
+    return (
         <ul>
             <li>Learn React</li>
             <li>Build Awesome App</li>
         </ul>
+    )
+};
+
+const AppHeader= () => {
+    return (
+        <h1>My ToDo List</h1>
+    )
+};
+
+const SearchPanel = () => {
+    return (
+        <input placeholder="search"/>
+    )
+};
+const el = (
+    <div>
+        <AppHeader />
+        <SearchPanel />
+        <TodoList />
     </div>
 );
 
