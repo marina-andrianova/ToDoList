960bc65dc8a09743107822ecf42166e5f04ddc01
marina-andrianova <andrianova12@bk.ru>
2019-11-06 14:20:03 +0300

    создание состояний у todo-list-item только в одну сторону(без отмены)

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -5,27 +5,56 @@ import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
 
-    constructor() { //используем конструктор только чтобы объявить внутри него функцию
-        super();
+    // constructor() { //используем конструктор только чтобы объявить внутри него функцию
+    //     super();
+    //     this.state = {//state только объект,чтобы я могла передать свойство состояния
+    //         done: false
+    //     };
+    //
+    //     this.onLabelClick = () => {//используем функции стрелки чтобы сохранить this(иначе получим работу с прототипом)
+    //         alert(`done: ${this.props.label}`)//this внутри функции стрелки сохраняет правильное значение
+    //     }
+    // }
+// ИЗБАВЛЯЮСЬ от конструктора и синтаксис теперь выглядит так:
+    state = {
+        done: false,
+        important: false
+    };
+    onLabelClick = () => {
+        this.setState({ //state изменяется ТОЛЬКО через setState
+            done: true,
+        })
+    };
+    onMarkImportant = () => {
+        this.setState({
+            important: true,
+        })
+    };
 
-        this.onLabelClick = () => {//используем функции стрелки чтобы сохранить this(иначе получим работу с прототипом)
-            alert(`done: ${this.props.label}`)//this внутри функции стрелки сохраняет правильное значение
-        }
-    }
 
     render() {
         const {label} = this.props;
+        const {done,important} = this.state;
 
-    return (
-        <span className="todo-list-item">
+        let classNames = 'todo-list-item';
+        if(done) {
+            classNames += ' done';
+        }
+        if (important) {
+            classNames += ' important';
+        }
+
+        return (
+            <span className={classNames}>
             <span
                 className="todo-list-item-label"
-                onClick={ this.onLabelClick }>
+                onClick={this.onLabelClick}>
             {label}
         </span>
             <button type="button"
-                    className="btn btn-outline-success btn-sm float-right">
-        <i className="fa fa-exclamation" />
+                    className="btn btn-outline-success btn-sm float-right"
+                    onClick={this.onMarkImportant}>
+        <i className="fa fa-exclamation"/>
       </button>
 
       <button type="button"
